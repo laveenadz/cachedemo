c5f8df45bb71e9e18fb61ccab52eaa07ef4b7f46
laveenadz <laveena.decipherzone@gmail.com>
2017-04-13 17:41:55 +0530

    Home screen icon

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,8 @@ const PRECACHE_URLS = [
   '/index.html',
   '/', // Alias for index.html
   '/image.jpg',
+  '/favicon.ico',
+  '/icon.png',
   '/sample-1.jpg',
   '/sample-2.jpg',
   '/pdf-sample.pdf',
