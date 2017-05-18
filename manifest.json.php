d8369290d9f8f436f57d4e9c6bbfefb8c340ae29
laveenadz <laveena.decipherzone@gmail.com>
2017-05-18 17:56:23 +0530

    Development test of service worker and application cache and manifest

--- manifest.json.php
+++ manifest.json.php
@@ -3,12 +3,9 @@
 header('Content-type: application/manifest+json');
 
 echo "{\"name\" : \"App BabsonChile trip\",\n";
-echo "\"short_name\": \"BabsonChile\",\n";
+echo "\"short_name\": \"App BabsonChile trip\",\n";
 echo "\"start_url\": \"/cachedemo\",\n";
-//echo "\"scope\": \"/cachedemo\",\n";
 echo "\"display\": \"standalone\",\n";
-echo "\"theme_color\": \"#9C2780\",\n";
-echo "\"background_color\": \"#58c9f3\",\n";
 echo "\"icons\": [";
 echo "{ \"src\": \"/cachedemo/icon.png\",\"sizes\": \"16x16\",\"type\": \"image/png\"},\n";
 echo "{ \"src\": \"/cachedemo/icon.png\",\"sizes\": \"32x32\",\"type\": \"image/png\"},\n";
