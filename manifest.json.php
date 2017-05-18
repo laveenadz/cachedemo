d889552a59e47e439ffe7282eaca724a87e5328c
laveenadz <laveena.decipherzone@gmail.com>
2017-05-18 16:17:12 +0530

    Test for dynamic manifest, service worker and application cache

--- manifest.json.php
+++ manifest.json.php
@@ -5,7 +5,7 @@
 echo "{\"name\" : \"App BabsonChile trip\",\n";
 echo "\"short_name\": \"BabsonChile\",\n";
 echo "\"start_url\": \"/cachedemo\",\n";
-echo "\"scope\": \"/cachedemo\",\n";
+//echo "\"scope\": \"/cachedemo\",\n";
 echo "\"display\": \"standalone\",\n";
 echo "\"theme_color\": \"#9C2780\",\n";
 echo "\"background_color\": \"#58c9f3\",\n";
